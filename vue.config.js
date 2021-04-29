4ae4317aaf8f4f89bde8f1e15251b539c2baa04c
gorden301 <1019108859@qq.com>
2021-04-29 16:42:57 +0800

    修复

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // 修改 src 目录 为 examples 目录
   pages: {
     index: {
-      entry: 'examples/main.ts',
+      entry: 'src/index.ts',
       template: 'public/index.html',
       filename: 'index.html',
     },
